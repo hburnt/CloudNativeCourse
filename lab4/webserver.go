b3147a1837c6138520ab517a7a11d546ae4a058b
hburnt <hwburnett1@gmail.com>
2024-02-18 20:49:41 -0500

    Added delete and read methods in webserver.go

--- lab4/webserver.go
+++ lab4/webserver.go
@@ -17,6 +17,8 @@ func main() {
 	//Additional Handlers
 	mux.HandleFunc("/update", db.update)
 	mux.HandleFunc("/create", db.create)
+	mux.HandleFunc("/read", db.read)
+	mux.HandleFunc("/delete", db.delete)
 	log.Fatal(http.ListenAndServe("localhost:8000", mux))
 }
 
@@ -30,19 +32,29 @@ type database struct {
 }
 
 func (db *database) list(w http.ResponseWriter, req *http.Request) {
+
+	//Lock Before Reading
+	db.mu.RLock()
 	for item, price := range db.data {
 		fmt.Fprintf(w, "%s: %s\n", item, price)
 	}
+	db.mu.RUnlock()
 }
 
 func (db *database) price(w http.ResponseWriter, req *http.Request) {
 	item := req.URL.Query().Get("item")
-	if price, ok := db.data[item]; ok {
-		fmt.Fprintf(w, "%s\n", price)
-	} else {
+
+	db.mu.RLock()
+	price, ok := db.data[item]
+	db.mu.Unlock()
+
+	if !ok {
 		w.WriteHeader(http.StatusNotFound) // 404
 		fmt.Fprintf(w, "no such item: %q\n", item)
 	}
+
+	fmt.Fprintf(w, "%s\n", price)
+
 }
 
 func (db *database) update(w http.ResponseWriter, req *http.Request) {
@@ -53,6 +65,7 @@ func (db *database) update(w http.ResponseWriter, req *http.Request) {
 
 	if err != nil {
 		fmt.Println("Error encountered: ", err)
+		fmt.Fprintf(w, "'%s' is not a valid price, please try again.\n", updatedPrice)
 		return
 	}
 	//Lock for reading
@@ -71,12 +84,13 @@ func (db *database) update(w http.ResponseWriter, req *http.Request) {
 	db.data[item] = dollars(price)
 	db.mu.Unlock()
 
-	fmt.Fprintf(w, "Updated %s's price to: %s\n", item, db.data[item])
+	fmt.Fprintf(w, "Updated the price for %s to %s\n", item, db.data[item])
 }
 
 func (db *database) create(w http.ResponseWriter, req *http.Request) {
 	item := req.URL.Query().Get("item")
 	newPrice := req.URL.Query().Get("price")
+
 	price, err := strconv.ParseFloat(newPrice, 32)
 
 	if err != nil {
@@ -88,5 +102,30 @@ func (db *database) create(w http.ResponseWriter, req *http.Request) {
 	db.data[item] = dollars(price)
 	db.mu.Unlock()
 
-	fmt.Fprintf(w, "New item added: Item: %s Price %s", item, db.data[item])
+	fmt.Fprintf(w, "New item added!\nItem Name: %s \nPrice: %s\n", item, db.data[item])
+}
+
+func (db *database) read(w http.ResponseWriter, req *http.Request) {
+	db.list(w, req)
+}
+
+func (db *database) delete(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+
+	db.mu.RLock()
+	_, ok := db.data[item]
+	db.mu.RUnlock()
+
+	if !ok {
+		w.WriteHeader(http.StatusNotFound) // 404
+		fmt.Fprintf(w, "no such item: %q\n", item)
+		return
+	}
+
+	// Lock for writing
+	db.mu.Lock()
+	delete(db.data, item)
+	db.mu.Unlock()
+
+	fmt.Fprintf(w, "Item Removed!\nItem Name: %s \n", item)
 }
